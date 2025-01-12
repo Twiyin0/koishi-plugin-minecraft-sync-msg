dfe7cb5f922c266eb63710c8ea499fbd0066b2df
unknown <yinlingcchan@outlook.com>
2025-01-12 16:20:53 +0800

    v2.0.2

--- src/index.tsx
+++ src/index.tsx
@@ -37,8 +37,10 @@ export const Config: Schema<Config> = Schema.intersect([
   }).description("基础配置")
 ] as const)
 
+// 全局存储 ws 实例
+let globalWs: WebSocket | undefined;
+
 export async function apply(ctx: Context, cfg: Config) {
-  let ws:WebSocket|undefined;
   const rcon = new Rcon({
     host: cfg.rconServerHost,
     port: cfg.rconServerPort,
@@ -50,24 +52,80 @@ export async function apply(ctx: Context, cfg: Config) {
     "Authorization": `Bearer ${cfg.Token}`,
     "x-client-origin": "koishi"
   };
-  let fork:any;
+  let fork: any;
 
   if (cfg.wsServer == '服务端') {
     fork = ctx.plugin(mcWss, cfg);
     return;
   } else {
-    ws = new WebSocket(`ws://${cfg.wsHost}:${cfg.wsPort}/minecraft/ws`, {
+    globalWs = new WebSocket(`ws://${cfg.wsHost}:${cfg.wsPort}/minecraft/ws`, {
       headers: headers
     });
+    bindWebSocketEvents(ctx, cfg, globalWs); // 绑定事件监听器
   }
 
   ctx.on('dispose', async () => {
     fork?.dispose()
-    fork? ctx.registry.delete(mcWss):undefined;
-    ws?.close();
+    fork ? ctx.registry.delete(mcWss) : undefined;
+    globalWs?.close();
+    clearReconnectInterval();
   })
 
-  ws?.on('open', function open() {
+  if (cfg.rconEnable) {
+    try {
+      await connectToRcon(rcon);
+    } catch (err) {
+      logger.error('RCON服务器连接失败');
+    }
+  }
+
+  ctx.on('message', async (session) => {
+    let imgurl: string;
+    if (session.content.includes('<img')) imgurl = h.select(session.content, 'img')[0].attrs.src;
+    if (cfg.sendToChannel.includes(`${session.platform}:${session.channelId}`) || session.platform == "sandbox") {
+      if ((session.content.startsWith(cfg.sendprefix)) && session.content != cfg.sendprefix) {
+        let msg: string = session.content.replaceAll('&amp;', '&').replaceAll(/<\/?template>/gi, '').replaceAll(cfg.sendprefix, '').replaceAll(/<img.*\/>/gi, `[[CICode,url=${imgurl}]]`);
+        try {
+          let msgData = {
+            "api": "send_msg",
+            data: {
+              "message": {
+                type: "text",
+                data: {
+                  text: `(${session.platform})[${session.event.user.name}] ` + extractAndRemoveColor(msg).output,
+                  color: extractAndRemoveColor(msg).color ? extractAndRemoveColor(msg).color : "white"
+                }
+              }
+            }
+          }
+          // 发送消息示例
+          globalWs?.send(JSON.stringify(msgData));
+        } catch (err) { logger.error(`[minecraft-sync-msg] 消息发送到WebSocket服务端失败`) }
+      }
+
+      if ((session.content.startsWith(cfg.cmdprefix)) && session.content != cfg.cmdprefix && cfg.rconEnable && cfg.cmdprefix) {
+        var cmd: string = session.content.replaceAll('&amp;', '§').replaceAll('&', '§').replaceAll(cfg.cmdprefix, '');
+        if (cfg.alluser) var res = await sendRconCommand(rcon, cmd);
+        else {
+          if (cfg.superuser.includes(session.userId)) {
+            var res = cfg.cannotCmd.includes(cmd) ? '危险命令，禁止使用' : await sendRconCommand(rcon, cmd);
+            res = res ? res : '该命令无反馈'
+          }
+          else if (cfg.commonCmd.includes(cmd)) {
+            var res = cfg.cannotCmd.includes(cmd) ? '危险命令，禁止使用' : await sendRconCommand(rcon, cmd);
+            res = res ? res : '该命令无反馈'
+          }
+          else var callbk = '无权使用该命令'
+        }
+        session.send(res ? res.replaceAll(/§./g, '') : callbk);
+      }
+    }
+  })
+}
+
+// 绑定 WebSocket 事件监听器
+function bindWebSocketEvents(ctx: Context, cfg: Config, ws: WebSocket) {
+  ws.on('open', function open() {
     logger.info('成功连上websocket服务器');
     if (!cfg.hideConnect) ctx.bots.forEach(async (bot: Bot) => {
       const channels = cfg.sendToChannel.filter(str => str.includes(`${bot.platform}`)).map(str => str.replace(`${bot.platform}:`, ''));
@@ -80,102 +138,52 @@ export async function apply(ctx: Context, cfg: Config) {
           type: "text",
           data: {
             text: extractAndRemoveColor(cfg.joinMsg).output,
-            color: extractAndRemoveColor(cfg.joinMsg).color? extractAndRemoveColor(cfg.joinMsg).color : "gold"
+            color: extractAndRemoveColor(cfg.joinMsg).color ? extractAndRemoveColor(cfg.joinMsg).color : "gold"
           }
         }
       }
     }
     // 发送消息示例
-    ws?.send(JSON.stringify(msgData));
+    ws.send(JSON.stringify(msgData));
   });
 
-  ws?.on('message', async (buffer)=> {
+  ws.on('message', async (buffer) => {
+    let sendImage:string = undefined;
     const data = JSON.parse(buffer.toString())
-    let eventName = data.event_name? getListeningEvent(data.event_name):'';
-    let sendMsg = getSubscribedEvents(cfg.event).includes(eventName)? `[${data.server_name}](${eventTrans[eventName].name}) ${eventTrans[eventName].action? data.player?.nickname+' ':''}${(eventTrans[eventName].action? eventTrans[eventName].action+' ':'')}${data.message? data.message:''}`:''
-    sendMsg = h.unescape(sendMsg).replaceAll('&amp;','&').replaceAll(/<\/?template>/gi,'').replaceAll(/§./g,'')
-    if(data.server_name)
+    let eventName = data.event_name ? getListeningEvent(data.event_name) : '';
+    let sendMsg = getSubscribedEvents(cfg.event).includes(eventName) ? `[${data.server_name}](${eventTrans[eventName].name}) ${eventTrans[eventName].action ? data.player?.nickname + ' ' : ''}${(eventTrans[eventName].action ? eventTrans[eventName].action + ' ' : '')}${data.message ? data.message : ''}` : ''
+    sendMsg = h.unescape(sendMsg).replaceAll('&amp;', '&').replaceAll(/<\/?template>/gi, '').replaceAll(/§./g, '');
+    if (sendMsg.match(/(https?|file):\/\/.*\.(jpg|jpeg|webp|ico|gif|jfif|bmp|png)/gi)) sendImage=sendMsg.match(/(https?|file):\/\/.*?\.(jpg|jpeg|webp|ico|gif|jfif|bmp|png)/i)?.[0]
+    sendMsg = sendImage? sendMsg.replace(sendImage, `<img src="${sendImage}" />`):sendMsg;
+    if (data.server_name)
       ctx.bots.forEach(async (bot: Bot) => {
         const channels = cfg.sendToChannel.filter(str => str.includes(`${bot.platform}`)).map(str => str.replace(`${bot.platform}:`, ''));
-        sendMsg? bot.broadcast(channels, sendMsg, 0):'';
+        sendMsg ? bot.broadcast(channels, sendMsg, 0) : '';
       });
-  })
-
-  // 连接关闭时的回调
-  ws?.on('close', async () => {
+  });
+ 
+  ws.on('close', async () => {
     if (!cfg.hideConnect) ctx.bots.forEach(async (bot: Bot) => {
       const channels = cfg.sendToChannel.filter(str => str.includes(`${bot.platform}`)).map(str => str.replace(`${bot.platform}:`, ''));
       bot.broadcast(channels, "与Websocket服务器断开连接!", 0);
     });
     logger.error('非正常与Websocket服务器断开连接!');
-    ws = undefined;
-    ws = await wsReconnect(cfg, headers) || undefined;
+    globalWs = undefined;
+    globalWs = await wsReconnect(cfg, {
+      "x-self-name": cfg.serverName,
+      "Authorization": `Bearer ${cfg.Token}`,
+      "x-client-origin": "koishi"
+    }, ctx) || undefined;
+    if (globalWs) bindWebSocketEvents(ctx, cfg, globalWs); // 重新绑定事件监听器
   });
 
-  // 连接错误时的回调
-  ws?.on('error', function error(err) {
+  ws.on('error', function error(err) {
     if (!cfg.hideConnect) ctx.bots.forEach(async (bot: Bot) => {
       const channels = cfg.sendToChannel.filter(str => str.includes(`${bot.platform}`)).map(str => str.replace(`${bot.platform}:`, ''));
       bot.broadcast(channels, "与Websocket服务器断通信时发生错误!", 0);
     });
-    logger.error('与Websocket服务器断通信时发生错误!'+err)
+    logger.error('与Websocket服务器断通信时发生错误!' + err)
   });
-
-  if (cfg.rconEnable) {
-    try {
-      await connectToRcon(rcon);
-    } catch(err) {
-      logger.error('RCON服务器连接失败');
-    }
-  }
-
-  // debug
-  // ctx.command('mcTest').action(async ({session})=>{
-  //   console.log(cfg.event);
-  //   console.log(getSubscribedEvents(cfg.event));
-  //   return getSubscribedEvents(cfg.event);
-  // })
-
-  ctx.on('message', async (session) => {
-  if (cfg.sendToChannel.includes(`${session.platform}:${session.channelId}`) || session.platform=="sandbox") {
-    if ((session.content.startsWith(cfg.sendprefix)) && session.content != cfg.sendprefix) {
-      let msg:string = session.content.replaceAll('&amp;', '&').replaceAll(/<\/?template>/gi,'').replaceAll(cfg.sendprefix,'');
-      try {
-        let msgData = {
-          "api": "send_msg",
-          data: {
-            "message": {
-              type: "text",
-              data: {
-                text: `(${session.platform})[${session.event.user.name}] `+extractAndRemoveColor(msg).output,
-                color: extractAndRemoveColor(msg).color? extractAndRemoveColor(msg).color : "white"
-              }
-            }
-          }
-        }
-        // 发送消息示例
-        ws?.send(JSON.stringify(msgData));
-      } catch (err) { logger.error(`[minecraft-sync-msg] 消息发送到WebSocket服务端失败`) }
-    }
-
-    if ((session.content.startsWith(cfg.cmdprefix)) && session.content != cfg.cmdprefix && cfg.rconEnable && cfg.cmdprefix) {
-      var cmd:string = session.content.replaceAll('&amp;','§').replaceAll('&','§').replaceAll(cfg.cmdprefix,'');
-      if (cfg.alluser) var res = await sendRconCommand(rcon,cmd);
-      else {
-        if (cfg.superuser.includes(session.userId)) {
-          var res = cfg.cannotCmd.includes(cmd)? '危险命令，禁止使用' : await sendRconCommand(rcon,cmd);
-          res = res? res:'该命令无反馈'
-        }
-        else if (cfg.commonCmd.includes(cmd)) {
-          var res = cfg.cannotCmd.includes(cmd)? '危险命令，禁止使用' : await sendRconCommand(rcon,cmd);
-          res = res? res:'该命令无反馈'
-        }
-        else var callbk = '无权使用该命令'
-      }
-      session.send(res? res.replaceAll(/§./g, '') : callbk);
-    }
-  }
-  })
 }
 
 // RCON部分
@@ -188,7 +196,7 @@ async function connectToRcon(rcon: any) {
   }
 }
 
-async function sendRconCommand(rcon:any, command:String) {
+async function sendRconCommand(rcon: any, command: String) {
   try {
     const response = await rcon.send(command);
     return response;
@@ -203,9 +211,9 @@ export function extractAndRemoveColor(input: string): { output: string, color: s
   const match = input.match(regex);
 
   if (match) {
-      const color = match[1];
-      const output = input.replace(regex, '');
-      return { output, color };
+    const color = match[1];
+    const output = input.replace(regex, '');
+    return { output, color };
   }
 
   return { output: input, color: '' };
@@ -215,7 +223,7 @@ let reconnectAttempts = 0; // 重连次数计数器
 let reconnectIntervalId: NodeJS.Timeout | null = null; // 用于存储 setInterval 的 ID
 
 // 重连函数
-async function wsReconnect(cfg: Config, headers: any): Promise<WebSocket | null | undefined> {
+async function wsReconnect(cfg: Config, headers: any, ctx?:Context): Promise<WebSocket | null | undefined> {
   let ws: WebSocket | undefined;
 
   if (reconnectIntervalId) return; // 如果已经存在定时器，则不再启动新的定时器
@@ -231,16 +239,20 @@ async function wsReconnect(cfg: Config, headers: any): Promise<WebSocket | null 
 
         ws.on('open', () => {
           logger.info('WebSocket 连接成功');
+          clearInterval(reconnectIntervalId);
+          bindWebSocketEvents(ctx,cfg,ws);
           clearReconnectInterval(); // 连接成功后清除定时器
+          globalWs = ws; // 更新全局 ws 实例
         });
 
         ws.on('error', () => {
           ws = undefined;
+          globalWs = undefined;
         });
 
         ws.on('close', () => {
           logger.info('WebSocket 连接关闭，开始重连...');
-          wsReconnect(cfg, headers); // 连接关闭后重新调用重连函数
+          wsReconnect(cfg, headers,ctx); // 连接关闭后重新调用重连函数
         });
 
       } catch (err) {
@@ -256,6 +268,7 @@ async function wsReconnect(cfg: Config, headers: any): Promise<WebSocket | null 
 }
 
 function clearReconnectInterval() {
+  reconnectAttempts = 0;
   if (reconnectIntervalId) {
     clearInterval(reconnectIntervalId);
     reconnectIntervalId = null;
